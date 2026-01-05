26a60fa8e3fd385d56d9a2ea474e4b3d8bf0d377
Arturo Ayala III <arturo-ayala@outlook.com>
2026-01-04 20:05:42 -0600

    Add region to organize

--- full-stack-app-demo.Server/Program.cs
+++ full-stack-app-demo.Server/Program.cs
@@ -6,10 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+#region Entity Framework Core Configuration
 builder.Services.AddDbContext<full_stack_app_demo.Server.Data.AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<full_stack_app_demo.Server.Data.Repositories.ICategoryRepository, full_stack_app_demo.Server.Data.Repositories.CategoryRepository>();
 builder.Services.AddScoped<full_stack_app_demo.Server.Data.Repositories.IProductRepository, full_stack_app_demo.Server.Data.Repositories.ProductRepository>();
+#endregion
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
